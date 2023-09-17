a585dd80cbb508816e1b66e3caba465223509448
mariusherlea <mariusherlea@gmail.com>
2023-09-17 09:56:42 +0300

    calculate estimated price for used vehicle

--- conditions/app.js
+++ conditions/app.js
@@ -47,4 +47,32 @@ const abs = (number) => {
 };
 
 let num = abs(-1);
-console.log(num);
+// console.log(num);
+
+// Buy Car and need license
+
+const display = (val) => {
+  console.log(val);
+};
+
+const needsLicense = (kind) => {
+  if (kind === "car" || kind === "truck") {
+    return true;
+  } else return false;
+};
+
+const chooseVehicle = (option1, option2) => {
+  let compareVehicle = option1.localeCompare(option2);
+  if (compareVehicle === -1) {
+    return option1;
+  } else if (compareVehicle === 1) {
+    return option2;
+  }
+};
+
+const calculateResellPrice = (originalPrice, age) => {
+  if (age < 3) return (originalPrice * 80) / 100;
+  else if (age > 10) return (originalPrice * 50) / 100;
+  else if ((age) => 3 && age < 10) return (originalPrice * 70) / 100;
+};
+display(calculateResellPrice(1000, 15));
