5aed908ad258154ee182f4edda85525eca787408
mariusherlea <mariusherlea@gmail.com>
2023-09-21 18:02:48 +0300

    https://exercism.org/tracks/javascript/exercises/mixed-juices ANOTHER APPROACH

--- while/app.js
+++ while/app.js
@@ -13,20 +13,20 @@
 // const result = countOfDivisors(2, 10);
 // console.log(result);
 
-const timeToMixJuice = (nameOfJuice) => {
-  switch (nameOfJuice) {
-    case "Pure Strawberry Joy":
-      return "0.5 minutes";
-    case "Energizer":
-      return "1.5 minutes";
-    case "Green Garden":
-      return "1.5 minutes";
-    case "Tropical Island":
-      return "3 minutes";
-    default:
-      return "2.5 minutes";
-  }
-};
+// const timeToMixJuice = (nameOfJuice) => {
+//   switch (nameOfJuice) {
+//     case "Pure Strawberry Joy":
+//       return "0.5 minutes";
+//     case "Energizer":
+//       return "1.5 minutes";
+//     case "Green Garden":
+//       return "1.5 minutes";
+//     case "Tropical Island":
+//       return "3 minutes";
+//     default:
+//       return "2.5 minutes";
+//   }
+// };
 
 // console.log(timeToMixJuice("Tropical Island"));
 // console.log(timeToMixJuice("Berries & Lime"));
@@ -35,41 +35,30 @@ const timeToMixJuice = (nameOfJuice) => {
 
 const limesToCut = (wedgesNeeded, limesStock) => {
   let i = 0;
-  console.log(wedgesNeeded);
-  while (wedgesNeeded >= 0) {
+  console.log("required: ", wedgesNeeded);
+  for (let i = 0; i < limesStock.length; i++) {
     if (limesStock[i] === "small") {
       limesStock[i] = 6;
-      wedgesNeeded -= limesStock[i];
-
-      i++;
     } else if (limesStock[i] === "medium") {
       limesStock[i] = 8;
-      wedgesNeeded -= limesStock[i];
-
-      i++;
     } else if (limesStock[i] === "large") {
       limesStock[i] = 10;
-      wedgesNeeded -= limesStock[i];
-
-      i++;
     }
   }
-  var result = limesStock.indexOf(limesStock[i]);
-  if (result < 0) {
-    return "out of range";
-  } else return result;
-};
-console.log(limesToCut(25, ["small", "small", "large", "medium", "small"]));
+  let sumOfLime = 0;
+  for (let i = 0; i < limesStock.length; i++) {
+    sumOfLime += limesStock[i];
+  }
+  console.log("array of lime:", limesStock);
+  console.log("limesStock: ", sumOfLime);
+  console.log("length of lime array", limesStock.length);
 
-const limesToCut2 = (wedgesNeeded, limesStock) => {
-  const arrayOfObjects = limesStock.map((str) => {
-    return { quantity: str, value: 10 };
-  });
-  arrayOfObjects.forEach((element) => {
-    if (element.quantity === "small") element.value = 6;
-    else if (element.quantity === "medium") element.value = 8;
-    else if (element.quantity === "large") element.value = 10;
-  });
-  return arrayOfObjects;
+  if (wedgesNeeded > sumOfLime) console.log("need more that we have");
+  else if (wedgesNeeded == sumOfLime) console.log("suntem perfecti");
+  else {
+    if (sumOfLime - wedgesNeeded < limesStock[limesStock.length]) {
+      console.log(limesStock[limesStock.length]);
+    } else console.log("dracu stie");
+  }
 };
-// console.log(limesToCut2(33, ["small", "small", "large", "medium", "small"]));
+limesToCut(30, ["small", "small", "large", "medium", "small"]); //6+6+10+8+6=36
