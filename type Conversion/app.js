c9d5f407892aefdeee9a0a00f580b9f788a9a016
mariusherlea <mariusherlea@gmail.com>
2023-09-23 13:16:51 +0300

    https://exercism.org/tracks/javascript/exercises/lucky-numbers number is palindrome

---
+++ type Conversion/app.js
@@ -0,0 +1,33 @@
+//https://exercism.org/tracks/javascript/exercises/lucky-numbers
+//Calculate the sum for the numbers on the slot machine
+const twoSum = (firstArray, secondArray) => {
+  let sumOfFirstArray,
+    sumOfSecondArray = 0;
+  for (i = 0; i <= 2; i++) {
+    sumOfFirstArray += String(firstArray[i]);
+    sumOfSecondArray += String(secondArray[i]);
+  }
+  console.log(
+    Number(removeCharFromString(sumOfFirstArray)) +
+      Number(removeCharFromString(sumOfSecondArray))
+  );
+};
+
+const removeCharFromString = (stringS1) => {
+  return stringS1.replace(new RegExp(`[${undefined}]`, "g"), "");
+};
+// twoSum([1, 2, 3], [0, 7]);
+// Determine if a number is a palindrome
+
+luckyNumber = (str) => {
+  let stringNumber = str.toString();
+  let reversed = "";
+  for (const char of stringNumber) {
+    reversed = char + reversed;
+  }
+  reversed = parseInt(reversed);
+
+  if (str === reversed) return "palindrome";
+  else return null;
+};
+console.log(luckyNumber(1442));
