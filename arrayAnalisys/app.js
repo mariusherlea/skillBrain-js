43f826bfc463ac05ceb30f183a1a5817bcddc6a1
mariusherlea <mariusherlea@gmail.com>
2023-09-25 13:26:37 +0300

    arrayAnalisys completed

--- arrayAnalisys/app.js
+++ arrayAnalisys/app.js
@@ -1,10 +1,19 @@
 const values = [9, 7, 3, 2];
 
-// Array#some stops iteration as soon as the given predicate returns true; it
-// returns false if none of the elements in the array pass the predicate. In
-// other words: return true if for some item the given function returns true.
-//
 getCardPosition = (values, card) => {
   return values.indexOf(card);
 };
-console.log(getCardPosition(values, 2));
+// console.log(getCardPosition(values, 2));
+const values2 = [2, 3, 4, 5];
+doesStackIncludeCard = (values, card) => {
+  return values.includes(card);
+};
+// console.log(doesStackIncludeCard(values2, 3));
+const valies = [3, 2, 6, 4, 8];
+const isOdd = (item) => item % 2 === 1;
+
+// console.log(valies.some(isOdd));
+
+const getFirstEvenCardPosition = (item) => item[1] % 2 !== 1;
+
+console.log(getFirstEvenCardPosition([5, 2, 3, 1]));
